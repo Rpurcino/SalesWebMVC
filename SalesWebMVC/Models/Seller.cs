8ba2626afb77e16800b23b491dc73691967f2de0
Rodrigo Purcino <rodrigo.purcino@yahoo.com.br>
2022-12-27 15:34:17 -0300

    validação sellers

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -7,15 +7,26 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required (ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
         public string Email { get; set; }
+
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "{0} required")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Base Salary")]
+        [Range(900, 20000, ErrorMessage ="{0} must be from {1} to {2}")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} required")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
